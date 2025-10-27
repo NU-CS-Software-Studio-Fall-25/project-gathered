db39e7dab1656fa12f20ea40316c1b5a92603a8e
Ellis Mandel <ellismandel@dhcp-10-105-110-21.wireless.northwestern.private>
2025-10-27 17:44:18 -0500

    Fix StudentSessionsController callback reference

--- app/controllers/student_sessions_controller.rb
+++ app/controllers/student_sessions_controller.rb
@@ -1,5 +1,5 @@
 class StudentSessionsController < ApplicationController
-  skip_before_action :ensure_current_student, only: :create
+  skip_before_action :authenticate_student!, only: :create
 
   def create
     session[:student_id] = Student.find_by(student_id: params[:student_id])&.student_id
