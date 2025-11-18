b6626229a4a5d88774f710d82fb9f91eec8b667c
Alex-Anca <alexanca2027@u.northwestern.edu>
2025-11-17 20:55:49 -0600

    Fixed db migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_16_000001) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_16_000002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,12 +41,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_16_000001) do
     t.string "name", limit: 100
     t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.string "email", null: false
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.string "avatar_color"
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_students_on_email", unique: true
-    t.index ["provider", "uid"], name: "index_students_on_provider_and_uid", unique: true, where: "(provider IS NOT NULL)"
+    t.index ["provider", "uid"], name: "index_students_on_provider_and_uid", unique: true
   end
 
   create_table "study_groups", primary_key: "group_id", force: :cascade do |t|
